1d6bd5c2f0bfabcbddc3eef9a7f53df1aa16ad47
Naveen Guleria <114605484+Gulerianaveen@users.noreply.github.com>
2024-08-27 20:27:01 +0000

    Data Types

---
+++ 01Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) we are using nodejs and not browser
+
+console.log(3+3);
+
+let name = "Naveen"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+
+//PRIMITIVE DATA TYPES
+// number => 2 to power 32
+// bigint
+// string => '' OR ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//NON PRIMITIVE DATA TYPES
+
+console.log(typeof name);
