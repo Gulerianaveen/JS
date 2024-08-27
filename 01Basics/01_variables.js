b70b8904c1510e7e6cb7e4998337ebb7f79fedf1
Naveen Guleria <114605484+Gulerianaveen@users.noreply.github.com>
2024-08-27 20:17:07 +0000

    Variables

---
+++ 01Basics/01_variables.js
@@ -0,0 +1,16 @@
+const account_Id = 12232 // value locked cannot be changed  
+let account_Email = "naveen38709@gmail.com"
+var account_Pass = "jdsjbhk"
+account_City = "Jaipur" // this is not safe
+let account_State
+
+// prfer not to use var bcz of scope and block scope
+//account_Id = 32
+
+account_Email = "hbas@gmail.com"
+account_Pass = "jnsjs"
+account_City = "Banglore"
+
+console.log(account_Id);
+console.table([account_Email,account_Id,account_Pass,account_City,account_State]);
+
